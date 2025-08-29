e84acb1644b7f77f384ffddfebc2ccb6e7963cac
Chamanth Krishna Yarlagadda <chamanthyarlagadda@gmail.com>
2025-08-29 19:54:22 +0530

    Added view resume and scroll to section functionality (#5)
    * Fixed deployment issue
    
    * Updated favicon, coming soon section
    
    * Added view resume and scroll to section functionality

--- src/constants/Common.ts
+++ src/constants/Common.ts
@@ -1,3 +1,4 @@
 export const emailId = 'chamanth@yarlagadda.dev';
 export const linkedinUrl = 'https://www.linkedin.com/in/chamanthyarlagadda/';
 export const githubUrl = 'https://github.com/ChamKrish';
+export const resumeFileId = '1htWjW_uoMXZVnBVmKT_Zjh1gAHn5iuRY'
